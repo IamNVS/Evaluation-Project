87894520d0f0ba120ccb4d160740ee65b371083f
Niraj Shegokar <136554711+IamNVS@users.noreply.github.com>
2025-02-20 22:55:56 +0530

    Update models.py

--- api/models.py
+++ api/models.py
@@ -8,7 +8,7 @@ class EvaluationRequest(models.Model):
     status = models.CharField( max_length=100, choices=
                               (("completed","completed"),
                                 ("pending", "pending")), default="pending") # Giving status and status of the evaluation (default is 'pending')
-    result = models.TextField # Stores the evaluation result
+    result = models.TextField(default="pending") # Stores the evaluation result
     created_at = models.DateTimeField(auto_now_add=True) # Timestamp when the evaluation request was created (set automatically)
     updated_at = models.DateTimeField(auto_now=True) # Timestamp when the evaluation request was last updated (updated automatically)
     email = models.EmailField() # User's email to send notifications once evaluation is completed
