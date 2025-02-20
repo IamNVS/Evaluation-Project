ffdff294527b89ef2f9aa90fb8d6e38446b55b7d
Niraj Shegokar <136554711+IamNVS@users.noreply.github.com>
2025-02-20 22:59:02 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 # Creating the endpoints
 urlpatterns = [
     path("api/evaluate/", SubmitEvaluationRequest.as_view(), name="submit-evaluation"),  # POST request, where submits an evaluation request and starts an asynchronous Celery task return the evaluation request ID
-    path("api/evaluate/<id>/", RetrieveEvaluationRequest.as_view(), name="retrieve-evaluation"),  # GET request ,<id> where you should add real ID to fetches the status and result of the evaluation
+    path("api/evaluate/<int:id>/", RetrieveEvaluationRequest.as_view(), name="retrieve-evaluation"),  # GET request ,<id> where you should add real ID to fetches the status and result of the evaluation
 ]
